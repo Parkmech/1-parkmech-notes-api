592dd7f10c3dd63f08d24d9bc598ddc9eb7e09b2
parker <mechamparker@gmail.com>
2022-01-15 22:18:54 -0700

    Javascript Spring Basic Project

---
+++ src/test/java/edu/byu/is590r/parkmech/notesapi/NotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.byu.is590r.parkmech.notesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
